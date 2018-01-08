70e950149012c117751a089155b52414b7d8a60e
mtryan <mtr3691@yahoo.com>
2018-01-08 17:00:32 -0600

    Added views folder, index view and app file.

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var redis = require('redis')
+
+var app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+//  res.send('Welcome!')
+  var title = 'Task List'
+  res.render('index', {
+    title:title
+  })
+})
+app.listen(3000)
+console.log('Server Started on Port 3000...')
+
+module.exports = app
