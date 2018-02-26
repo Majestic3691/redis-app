5b0f4b1f6e4e6256877e250a06d2ab4e2c5ec1d1
Michael Ryan <mtr3691@yahoo.com>
2018-02-26 15:08:58 -0600

    Added tasks to the dockerfile and commands to the shell script

--- config.js
+++ config.js
@@ -3,9 +3,10 @@ var config = {
 //  secret: 'secret',
 
   redisConf: {
-    host: '192.168.1.187', // The redis's server ip
+    host: '192.168.1.210', // The redis's server ip
     port: '6379',
-//    def_host: '127.0.0.1', // The redis's server ip
+    //    def_host: 'MAJESTIC8570W2', // The redis's server ip
+    //    def_host: '127.0.0.1', // The redis's server ip
 //    def_port: '6379'
     pass: 'majestic'
   }
--- data/LoadData.sh
+++ data/LoadData.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-cat /data/redis/StateAirportsSetReady.data | '/usr/local/bin/redis-cli --pipe'
+
+/usr/local/bin/redis-cli flushall async
+
+cat /data/redis/StateAirportsSetReady.data | /usr/local/bin/redis-cli --pipe
 cat /data/redis/StatesReady.data | /usr/local/bin/redis-cli --pipe
-cat /data/redis/TaskReady.data | '/usr/local/bin/redis-cli --pipe'
-cat /data/redis/GEOAirportDataReady.data | '/usr/local/bin/redis-cli --pipe'
-cat /data/redis/CallReady.data | '/usr/local/bin/redis-cli --pipe'
+cat /data/redis/TaskReady.data | /usr/local/bin/redis-cli --pipe
+cat /data/redis/GEOAirportDataReady.data | /usr/local/bin/redis-cli --pipe
+cat /data/redis/CallReady.data | /usr/local/bin/redis-cli --pipe
