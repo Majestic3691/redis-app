9ba84f55b1872d5f67deedaae995c9b2c2d21feb
Michael Ryan <mtr3691@yahoo.com>
2018-02-28 15:29:20 -0600

    Added backup db. Moved files to the /usr/share/redis/data folder. Disabled appendAOF.

--- createDocker.cmd
+++ createDocker.cmd
@@ -1,6 +1,6 @@
 REM * Cleanup
 docker images prune -q
-docker volume prune -f
+REM * docker volume prune -f
 docker container prune -f
 REM * Stop Container
 docker stop redis-svr
@@ -9,7 +9,7 @@ docker rm -f redis-svr
 REM * Remove Image
 docker image rm -f redisbase
 REM * Create the image and container - deploy
-docker volume create --name redis-data
-COPY ./data/*.data redis-data
-docker build -t redisbase --iidfile imageID -f docker/Dockerfile .
+REM * docker volume create --name redis-data
+REM * COPY ./data/*.data redis-data
+docker build --rm --tag redisbase --iidfile imageID --file docker/Dockerfile .
 docker run -d --name redis-svr -p6379:6379 -v redis-data:/usr/share/redis/data redisbase
